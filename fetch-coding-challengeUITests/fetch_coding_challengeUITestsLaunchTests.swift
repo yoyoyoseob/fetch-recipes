625a24c2b29c8f63038039ce7e4bac7292eab327
yoyoyoseob <yoyoyoseob@gmail.com>
2024-08-26 10:49:51 -0400

    Create Xcode project

---
+++ fetch-coding-challengeUITests/fetch_coding_challengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  fetch_coding_challengeUITestsLaunchTests.swift
+//  fetch-coding-challengeUITests
+//
+//  Created by Yoseob Lee on 8/26/24.
+//
+
+import XCTest
+
+final class fetch_coding_challengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
